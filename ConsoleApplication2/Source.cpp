5632714c9fba1bd6a60e5d43055108a5297aa2b6
Jaroslawkolaczek <j-kolaczek@wp.pl>
2015-11-25 17:06:09 +0100

    Plansza + czolgi

--- ConsoleApplication2/Source.cpp
+++ ConsoleApplication2/Source.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <allegro5\allegro.h>
+#include <allegro5\allegro_image.h>
 #include <allegro5\allegro_primitives.h>
 #include <allegro5\allegro_font.h>
 #include <allegro5\allegro_ttf.h>
@@ -52,59 +53,122 @@ int main(){
 		{
 		case 1:
 		{
-			int width = 640;
-			int height = 480;
+
 			bool done = false;
-
-			int pos_x = width / 2;
-			int pos_y = height / 2;
-
-			ALLEGRO_DISPLAY *display = NULL;
-			ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-
-
-
+			al_init_primitives_addon();
+			int poz_x = 400;
+			int poz_y = 400;
+			int poz_x2 = 300;
+			int poz_y2 = 300;
+			int counter = 0;
+			const int FPS = 60;
 			if (!al_init()){
-				al_show_native_message_box(NULL, NULL, NULL, "failed to initialize allegro!", NULL, NULL);
+				fprintf(stderr, "Failed to initialize Allegro.\n");
 				return -1;
 			}
 
-			display = al_create_display(width, height);
+			al_init_font_addon(); // initialize the font addon
+			al_init_ttf_addon();// initialize the ttf (True Type Font) addon
+			al_init_image_addon();
+			ALLEGRO_DISPLAY *display = al_create_display(800, 600);
+			ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+			ALLEGRO_BITMAP *czolk = NULL;
+			ALLEGRO_BITMAP *czolk2 = NULL;
+			ALLEGRO_BITMAP *tlo = NULL;
 
 			if (!display){
-				al_show_native_message_box(NULL, NULL, NULL, "failed to initialize allegro!", NULL, NULL);
+				fprintf(stderr, "Failed to create display.\n");
+				return -1;
+			}
+			ALLEGRO_TIMER *czas = NULL;
+			czas = al_create_timer(1.0 / FPS);
+			ALLEGRO_FONT *font = al_load_ttf_font("comic.ttf", 36, 0);
+			ALLEGRO_FONT *font2 = al_load_ttf_font("comic.ttf", 12, 0);
+			if (!font){
+				fprintf(stderr, "Could not load 'comic.ttf'.\n");
 				return -1;
 			}
 
-			al_init_font_addon();
-			al_init_ttf_addon();
-			al_init_primitives_addon();
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+
 			al_install_keyboard();
+
 
 			event_queue = al_create_event_queue();
 			al_register_event_source(event_queue, al_get_keyboard_event_source());
+			al_register_event_source(event_queue, al_get_timer_event_source(czas));
+			al_start_timer(czas);
+			czolk = al_load_bitmap("czolk.bmp");
+			al_convert_mask_to_alpha(czolk, al_map_rgb(255, 255, 255));
+			czolk2 = al_load_bitmap("czolk2.bmp");
+			al_convert_mask_to_alpha(czolk2, al_map_rgb(255, 255, 255));
+			tlo = al_load_bitmap("tlo.bmp");
+			al_convert_mask_to_alpha(tlo, al_map_rgb(255, 255, 255));
 
+		
 			while (!done)
-			{
-				al_draw_filled_rectangle(pos_x, pos_y, pos_x + 30, pos_y + 30, al_map_rgb(255, 0, 0));
-				al_flip_display();
-				al_clear_to_color(al_map_rgb(0, 0, 0));
+			{			
 
-				ALLEGRO_EVENT ev;
-				al_wait_for_event(event_queue, &ev);
+					al_draw_bitmap(czolk, poz_x, poz_y, 0);
+					al_flip_display();
+					al_clear_to_color(al_map_rgb(0, 0, 0));
+					ALLEGRO_EVENT ev;
+					
+					al_wait_for_event(event_queue, &ev);
+					al_draw_textf(font2, al_map_rgb(0, 255, 0), 50, 5, ALLEGRO_ALIGN_CENTRE, "Odswiezanie: %i", counter);
 
-				if (ev.type == ALLEGRO_EVENT_KEY_UP&&ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
-					done = true;
-				if (ev.type == ALLEGRO_EVENT_KEY_DOWN && ev.keyboard.keycode == ALLEGRO_KEY_RIGHT)
-					pos_x += 30;
-				if (ev.type == ALLEGRO_EVENT_KEY_DOWN && ev.keyboard.keycode == ALLEGRO_KEY_LEFT)
-					pos_x -= 30;
-				if (ev.type == ALLEGRO_EVENT_KEY_DOWN && ev.keyboard.keycode == ALLEGRO_KEY_UP)
-					pos_y -= 30;
-				if (ev.type == ALLEGRO_EVENT_KEY_DOWN && ev.keyboard.keycode == ALLEGRO_KEY_DOWN)
-					pos_y += 30;
-			}
+					al_draw_bitmap(tlo, 0, 0, 0);
+					al_flip_display();
+					al_clear_to_color(al_map_rgb(0, 0, 0));
 
+					al_wait_for_event(event_queue, &ev);
+					al_draw_textf(font2, al_map_rgb(0, 255, 0), 50, 5, ALLEGRO_ALIGN_CENTRE, "Odswiezanie: %i", counter);
+					if (ALLEGRO_EVENT_KEY_UP && ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+						done = true;
+					if (ev.type == ALLEGRO_EVENT_KEY_DOWN){
+						switch (ev.keyboard.keycode)
+						{
+
+						case ALLEGRO_KEY_RIGHT: poz_x += 25; break;
+
+						case ALLEGRO_KEY_LEFT: poz_x -= 25; break;
+
+						case ALLEGRO_KEY_UP: poz_y -= 25; break;
+
+						case ALLEGRO_KEY_DOWN: poz_y += 25; break;
+
+						}
+					}
+					al_draw_bitmap(czolk2, poz_x2, poz_y2, 0);
+					al_flip_display();
+					al_clear_to_color(al_map_rgb(0, 0, 0));
+					
+					al_wait_for_event(event_queue, &ev);
+					al_draw_textf(font2, al_map_rgb(0, 255, 0), 50, 5, ALLEGRO_ALIGN_CENTRE, "Odswiezanie: %i", counter);
+					if (ALLEGRO_EVENT_KEY_UP && ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+						done = true;
+					if (ev.type == ALLEGRO_EVENT_KEY_DOWN){
+						switch (ev.keyboard.keycode)
+						{
+
+						case ALLEGRO_KEY_D: poz_x2 += 25; break;
+
+						case ALLEGRO_KEY_A: poz_x2 -= 25; break;
+
+						case ALLEGRO_KEY_W: poz_y2 -= 25; break;
+
+						case ALLEGRO_KEY_S: poz_y2 += 25; break;
+
+						}
+					}
+
+
+					counter++;
+				}
+						
+			al_flip_display();
+			al_destroy_event_queue(event_queue);
+			al_destroy_display(display);
 
 			return 0;
 		}
